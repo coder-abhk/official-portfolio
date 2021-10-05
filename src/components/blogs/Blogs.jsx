c30062e996a7c4fd5eb0b53eac7d1bf8ff40d577
coder-abhk <abhkrwt07@gmail.com>
2021-10-05 12:32:48 +0530

    new blogs added

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -20,12 +20,35 @@ function Blogs() {
         </li>
         <li>
           <a
+            href="https://channel-live.herokuapp.com/details/15"
+            alt="How to move a file from one folder to another using linux/ubuntu terminal?"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <em>
+              How to move a file from one folder to another using linux/ubuntu
+              terminal?
+            </em>
+          </a>
+        </li>
+        <li>
+          <a
             href="https://channel-live.herokuapp.com/details/13"
             alt="make your code look elegant in VSCode?"
             target="_blank"
             rel="noreferrer"
           >
             <em>Make your code look elegant in VSCode?</em>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://channel-live.herokuapp.com/c_sql/"
+            alt="CRUD operations in sql or mysql?"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <em>CRUD operations in sql/mysql?</em>
           </a>
         </li>
         <li>
