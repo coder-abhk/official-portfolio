0befc1184c151a181a7c8d5795e561ca9037a161
coder-abhk <abhkrwt07@gmail.com>
2023-09-23 12:03:02 +0530

    smooth scroll

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,9 @@ import Skills from "./Skills";
 import Projects from "./Projects";
 import Footer from "./Footer";
 import Blogs from "./blogs/Blogs";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Other from "./Other";
+import Lenis from "@studio-freight/lenis";
 
 function App() {
   const [isToggle, setIsToggle] = useState(false);
@@ -14,6 +15,17 @@ function App() {
   isToggle
     ? classDims.map((ele) => (ele.style.opacity = 0.25))
     : classDims.map((ele) => (ele.style.opacity = 1));
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  });
   return (
     <div className="app__wrapper">
       <Header setIsToggle={setIsToggle} isToggle={isToggle} />
